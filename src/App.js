79e67759dcd6e3da17345f17435ce15e9843df06
Samuel Askeli <samuel.askeli@gmail.com>
2022-01-20 18:31:36 +0200

    add placeholder fetch logic and creat ea hook that uses context.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useContext, useState } from 'react';
+
+
+// Context used solely for transferring knowledge of which cities are selected
+// from dropdown to WeatherList
+const selectionContext = React.createContext({
+  selection: "Kaikki kaupungit",
+  setSelection: () => { },
+});
+
+// Cities to be picked from, first option is all cities because this structure
+// is fed directly into the dropdown menu. Could be done otherwise.
+const cities = [
+  "Kaikki kaupungit",
+  "Helsinki",
+  "Jyväskylä",
+  "Kuopio",
+  "Tampere",
+];
 
 function App() {
+  //const [selection, setSelection] = useState(cities[0]);
+  //const value = { selection, setSelection };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+              <div className="App-header">
+      <h2> Hello, Sailor!</h2>
+
+    </div>
+    <selectionContext.Provider value={{
+  selection: "Kaikki kaupungit",
+  setSelection: () => { },
+}}>
+      <ContextCheck/>
+    </selectionContext.Provider>
     </div>
   );
 }
 
+
+const ContextCheck = () => {
+  const value = useContext(selectionContext);
+  //const [selection, setSelection] = useContext(selectionContext);
+  return (
+    <div>
+      <p>Current selection as according to context: {value.selection}</p>
+      </div>
+  )
+}
 export default App;
